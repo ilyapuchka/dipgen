50b37ae18960c9454df81a294046ea15cf3e8e6a
Ilya Puchka <ilyapuchka@gmail.com>
2016-10-01 17:02:44 +0200

    updated dependencies

--- Carthage/Checkouts/SourceKitten/Carthage/Checkouts/Commandant/Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-  name: "Commandant",
-  dependencies: [
-    .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 2)
-  ]
-)
